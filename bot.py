87c3ff9c178d9856e05ece193521004a0d0b8412
wsricardo <wandeson.math@gmail.com>
2021-03-13 23:31:18 -0300

    Start project

---
+++ bot.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# pylint: disable=W0613, C0116
+# type: ignore[union-attr]
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to reply to Telegram messages.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+"""
+    Get data from files with output messages for bot.
+    Using database mongodb or sql and json file.
+"""
+
+import logging
+import json
+
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Get data file for infos
+def getData():
+    pass
+
+# Natural Language input/output
+def nlOut(msg: str) -> str:
+    return 'x'
+
+# Consulta
+def consult(update: Update, context: CallbackContext) -> str:
+    return ''
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+def speak(inbot: str, context: CallbackContext) -> None:
+    outbot = ''
+    update.message.reply_text(outbot)
+
+def getName(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("WandriBot")
+
+
+def main():
+    """Start the bot."""
+    import configBot
+    print(configBot.confbot['bot_name'],configBot.confbot['info']) 
+    
+    # Create the Updater and pass it your bot's token.
+    #updater = Updater("TOKEN")
+    updater = Updater(configBot.confbot['token'])
+
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("name", getName))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ configBot.py
@@ -0,0 +1,11 @@
+confbot = {
+        "bot_name":"",
+        "token":"your_token",
+        "descrition": "Assistente virtual. Chatbot",
+        "info":  [
+            {"age":""},
+            {"persona":""}
+            ]
+
+        }
+            
